c4ad42b9a65be78e2bd79b306f471cc619f5b4b5
ama09 <ama09@ELEC-DOUBLE-REC>
2024-07-30 16:53:46 -0400

    Add project files.

---
+++ webmaster thing 2/wwwroot/navbar_thing.js
@@ -0,0 +1,27 @@
+﻿//// -------- STEP 1 -------- //
+//const _tabs = document.querySelectorAll('[data-tab]');
+//const _content = document.getElementsByClassName('active');
+
+//// -------- STEP 3 -------- //
+//const toggleContent = function () {
+
+//    // Part One
+//    if (!this.classList.contains("active")) {
+
+//        Array.from(_content).forEach(item => {
+//            item.classList.remove('active');
+//        });
+
+//        this.classList.add('active');
+
+//        // Part Two
+//        let currentTab = this.getAttribute('data-tab'),
+//            _tabContent = document.getElementById(currentTab);
+//        _tabContent.classList.add('active');
+//    }
+//};
+
+//// -------- STEP 2 -------- //
+//Array.from(_tabs).forEach(item => {
+//    item.addEventListener('click', toggleContent);
+//});
---
+++ webmaster thing 2/wwwroot/parallax_thing.js
@@ -0,0 +1,4 @@
+﻿window.addEventListener("scroll", function () {
+    const distance = window.scrollY;
+    document.querySelector("header").style.transform = `translateY(${distance * 0.7}px)`;
+});
